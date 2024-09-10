5dceff4a67ab3b5284b09eb75e23bd503adccdd2
Victor <paulo9.victor.silva@gmail.com>
2024-09-10 12:53:47 -0300

    chore: create initial swagger config

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
 import { router } from './routes';
 
 const port = process.env.PORT || '3333';
 const app = express();
 
+router.use('/swagger', swaggerUi.serve);
+router.get('/swagger', swaggerUi.setup(swaggerDocument));
+
 app.use(express.json());
-
 app.use(router);
-
 app.listen(port, () => { console.log(`Server running on port: ${port}`); });
