8bc0a5069b072a52b8f63a60cdbcb8bfc52c62c7
Victor <paulo9.victor.silva@gmail.com>
2024-09-10 17:45:34 -0300

    feat: create user route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import { UserController } from "./controllers/userController";
 
 const router = Router();
+const userController = new UserController();
 
-router.get('/', (_, res) => {
+router.post('/auth/create-user', userController.create);
+
+router.get('/auth/login', (_, res) => {
     return res.send('Olá, DEV!');
 });
 
