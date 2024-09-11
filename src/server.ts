8f42e69e355dffd61265afa0ef034fe8cf79a6e0
Victor <paulo9.victor.silva@gmail.com>
2024-09-11 01:58:42 -0300

    feat: cors config

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
 
@@ -7,12 +8,19 @@ import swaggerDocument from './swagger.json';
 import { router } from './routes';
 
 const port = process.env.PORT || '3333';
+const corsOptions = {
+    origin: '*',
+    methods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
 const app = express();
 
 app.use('/swagger', swaggerUi.serve);
 app.get('/swagger', swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use('/api', router);
+
 app.listen(port, () => { console.log(`Server running on port: ${port}`); });
 
