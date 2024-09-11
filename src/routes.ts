a86991ed03f0823f92e5052a82076afc332bacb2
Victor <paulo9.victor.silva@gmail.com>
2024-09-11 01:07:03 -0300

    feat: add authControlller and authMiddleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { UserController } from "./controllers/userController";
+import { AuthController } from "./controllers/authController";
+import { authMiddleware } from "./middleware/authMiddleware";
 
 const router = Router();
 const userController = new UserController();
+const authController = new AuthController();
 
+router.post('/auth/login', authController.login);
+
+router.use(authMiddleware);
+
+router.get('/auth/profile', authController.getProfile);
 router.post('/auth/create-user', userController.create);
-
-router.get('/auth/login', (_, res) => {
-    return res.send('Olá, DEV!');
-});
 
 export { router };
