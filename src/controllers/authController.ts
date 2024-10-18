815433efcc777bd60f2d259673f1cac7eb5b881f
Victor <paulo9.victor.silva@gmail.com>
2024-10-18 04:15:00 -0300

    fix: remove user endpoint return

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -24,11 +24,8 @@ export class AuthController {
                     expiresIn: '8h',
                 });
 
-                console.log(user);
-
                 return res.json({
-                    token: token,
-                    user: user
+                    token: token
                 });
 
             }
