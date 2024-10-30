4be26a8869d7be751461337d0e3b8dffa0c363e9
Victor <paulo9.victor.silva@gmail.com>
2024-10-30 18:32:43 -0300

    fix: get products

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -3,7 +3,25 @@ import { prismaClient } from "../database/prismaClient";
 export class ProductController {
     async getProducts(req: Request, res: Response) {
         try {
-            const products = await prismaClient.product.findMany();
+            const products = await prismaClient.product.findMany({
+                select: {
+                    name: true,
+                    Category: {
+                        select: {
+                            name: true,
+                            Product: true
+                        }
+                    },
+                    ProductItem: {
+                        select: {
+                            Color: true,
+                            Product: true,
+                            sale_price: true,
+                            SizeOption: true,
+                        }
+                    }
+                }
+            });
             return res.json(products);
         } catch (error) {
             return res.status(500).json({ error: "Failed to fetch products" });
@@ -13,8 +31,8 @@ export class ProductController {
     async registerProduct(req: Request, res: Response) {
         try {
             const { name, category_id } = req.body;
-            console.log('name', name)
-            console.log('category_id', category_id)
+            console.log('name', name);
+            console.log('category_id', category_id);
 
             if (!name || !category_id) {
                 return res.status(400).json({ error: "Name and category_id are required" });
