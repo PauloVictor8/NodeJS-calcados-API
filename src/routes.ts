e1116388780c01e927fe05e1d8ae47d020fa9494
Laines Rodrigues - neWave Telecom <laines.rodrigues@newavetelecom.com.br>
2024-10-30 08:57:53 -0300

    rota get all

--- src/routes.ts
+++ src/routes.ts
@@ -24,6 +24,7 @@ router.get('/manage/products', productController.getProducts);
 
 router.get('/manage/products', productController.getProducts);
 router.post('/manage/products/register', productController.registerProduct);
+router.get('/manage/products/getall', productController.getProducts);
 
 export { router };
 
