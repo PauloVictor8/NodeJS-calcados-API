0c31380df40e9fc335f40e2eb71d25a30ef02385
Laines Rodrigues - neWave Telecom <laines.rodrigues@newavetelecom.com.br>
2024-10-30 13:04:35 -0300

    migrate categories and remove logs controller product

---
+++ prisma/migrations/20241030567564_insert_categories/migration.sql
@@ -0,0 +1,13 @@
+INSERT INTO "Category" (name) VALUES
+('plataforma'),
+('rasteirinha'),
+('salto'),
+('tenis'),
+('bota');
+-- Insert
+INSERT INTO "Category" (name) VALUES
+('plataforma'),
+('rasteirinha'),
+('salto'),
+('tenis'),
+('bota');
--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -13,20 +13,10 @@ export class ProductController {
     async registerProduct(req: Request, res: Response) {
         try {
             const { name, category_id } = req.body;
-            console.log('name', name)
-            console.log('category_id', category_id)
 
             if (!name || !category_id) {
                 return res.status(400).json({ error: "Name and category_id are required" });
             }
-
-            // const name_category = String(category_id);
-            // await prismaClient.category.create({
-            //     data: {
-            //         category_id: category_id,
-            //         name: name_category
-            //     }
-            // })
 
             const newProduct = await prismaClient.product.create({
                 data: {
